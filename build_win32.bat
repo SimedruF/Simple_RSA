f91240730060712deaca36fbd1290ce6a31d90fe
Simedru Florin <simedruflorin@yahoo.com>
2024-01-24 16:39:28 +0200

    build corrected

--- build_win32.bat
+++ build_win32.bat
@@ -2,8 +2,8 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 @set OUT_DIR=Debug
 @set OUT_EXE=example_win32_directx9
-@set INCLUDES=/I\imgui\ /I\imgui\backends\ /I "%DXSDK_DIR%/Include"
-@set SOURCES=main.cpp RSAApp.cpp \imgui\backends\imgui_impl_dx9.cpp \imgui\backends\imgui_impl_win32.cpp \imgui\imgui*.cpp
+@set INCLUDES=/I.\imgui\ /I.\imgui\backends\ /I "%DXSDK_DIR%/Include"
+@set SOURCES=main.cpp RSAApp.cpp .\imgui\backends\imgui_impl_dx9.cpp .\imgui\backends\imgui_impl_win32.cpp .\imgui\imgui*.cpp
 @set LIBS=/LIBPATH:"%DXSDK_DIR%/Lib/x86" d3d9.lib
 mkdir %OUT_DIR%
 cl /nologo /Zi /MD /utf-8 %INCLUDES% /D UNICODE /D _UNICODE %SOURCES% /Fe%OUT_DIR%/%OUT_EXE%.exe /Fo%OUT_DIR%/ /link %LIBS%
